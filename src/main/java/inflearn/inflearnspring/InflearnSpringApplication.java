3ea8d101d15bb50ba39ee8802ef1455b53e259c4
subinmun1997 <qzxy812@gmail.com>
2021-12-30 16:11:38 +0900

    Initial commit

---
+++ src/main/java/inflearn/inflearnspring/InflearnSpringApplication.java
@@ -0,0 +1,13 @@
+package inflearn.inflearnspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InflearnSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InflearnSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/inflearn/inflearnspring/InflearnSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package inflearn.inflearnspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
