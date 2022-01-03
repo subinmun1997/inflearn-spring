99a4b8915b1644a65d82cb2fca47237fd56fbe58
subinmun1997 <qzxy812@gmail.com>
2022-01-03 17:02:33 +0900

    5. 회원 관리 예제-웹 MVC 개발

---
+++ src/main/java/inflearn/inflearnspring/controller/MemberForm.java
@@ -0,0 +1,14 @@
+package inflearn.inflearnspring.controller;
+
+public class MemberForm {
+
+    private String name; // alt + insert = getter & setter
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
