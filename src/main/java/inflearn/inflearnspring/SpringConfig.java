046c7a391ba12e7c96dd212e1fc25bc07f80ac6f
subinmun1997 <qzxy812@gmail.com>
2022-01-08 21:39:36 +0900

    6. 스프링 DB 접근 기술

--- src/main/java/inflearn/inflearnspring/SpringConfig.java
+++ src/main/java/inflearn/inflearnspring/SpringConfig.java
@@ -1,13 +1,24 @@
 package inflearn.inflearnspring;
 
+import inflearn.inflearnspring.repository.JdbcMemberRepository;
+import inflearn.inflearnspring.repository.JdbcTemplateMemberRepository;
 import inflearn.inflearnspring.repository.MemberRepository;
-import inflearn.inflearnspring.repository.MemoryMemberRepository;
 import inflearn.inflearnspring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +27,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        //return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 }
